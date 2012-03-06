5b726ca779c77e2c7b897d970b63d1ed37f7524c
Søren Louv-Jansen <sl@konscript.com>
2012-03-06 17:33:19 +0100

    Updated auth plugin

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -16,42 +16,49 @@ class AppController extends Controller {
     }
     
     function isAuthorized(){ 
-    
-    debug($this->Auth->user('role_id'));
-		// allow admins access to everything
+
+        // allow executives access to everything
         if($this->Auth->user('role_id') == '1'){
-			return true; 
-		}			 
-		
-		// default permissions
-		$permissions_default = array(
-			'view' => '*',
-			'index' => '*'
-		);     
-		
-		// override default perms
-		$permissions = array_merge($permissions_default, $this->permissions);
-		
-		// check permission for action 
-        if(!empty($permissions[$this->action])){ 
+            return true; 
+        }            
         
-        	// access for everyone (all logged in users)
-            if($permissions[$this->action] == '*'){ 
-            	return true;               
-        	}
-               
-           // 
-			$roles = array(
-				1 => 'admin',
-				2 => 'editor',
-				3 => 'visitor'
-			);
-            $role_alias = $roles[$this->Auth->user('role_id')];
-            
-            // access for specific role
-            if(in_array($role_alias, $permissions[$this->action])) return true; 
+        // roles
+        $roles = array(
+            1 => 'admin',
+            2 => 'group_manager', 
+            3 => 'project_manager',
+            4 => 'electrician',
+            5 => 'rundering'
+        );
+
+        // default permissions - everyone has access to view, index and add
+        $permissions_default = array(
+            'view' => '*',
+            'index' => '*'
+        );     
+
+        // override default permissions
+        $permissions = array_merge($permissions_default, $this->permissions);       
+
+        // get current role_alias eg. agent
+        $role_alias = $roles[$this->Auth->user('role_id')];
+
+        debug($role_alias);
+
+        // convert string to array        
+        if(isset($permissions[$this->action])){
+            $current_permission = $permissions[$this->action];
+            $allowedRoles = is_string($current_permission) ? array($current_permission) : $current_permission;
+        }else{
+            $allowedRoles = array();
+        }
+        
+        // give access, if role_alias has permissions to the current action. eg. agent (role_alias) can delete (action) users        
+        if( in_array($role_alias, $allowedRoles) || in_array('*', $allowedRoles) ){
+            return true; 
         } 
-        return false; 
-         
+            
+        // access denied
+        return false;
     }     
 }
